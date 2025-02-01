6c0833515da0b2c852f4dd0b3631122a72fa0fd7
declan308 <declancorner@gmail.com>
2025-02-01 12:47:52 +1000

    second commit - black screen loaded

--- main.py
+++ main.py
@@ -6,8 +6,17 @@ from constants import *
 
 def main():
     print("Starting asteroids!")
+    pygame.init()
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    color = (0,0,0)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, color)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
